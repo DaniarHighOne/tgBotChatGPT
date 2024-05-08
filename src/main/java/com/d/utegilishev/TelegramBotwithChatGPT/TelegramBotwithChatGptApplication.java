ce079d16b92decc7b524a0f8e776129b47a121bc
Daniar <daniyaruteg123@gmail.com>
2024-05-08 13:12:00 +0500

    commits

---
+++ src/main/java/com/d/utegilishev/TelegramBotwithChatGPT/TelegramBotwithChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.d.utegilishev.TelegramBotwithChatGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotwithChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotwithChatGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d/utegilishev/TelegramBotwithChatGPT/TelegramBotwithChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d.utegilishev.TelegramBotwithChatGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotwithChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
